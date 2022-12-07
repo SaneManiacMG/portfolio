9afad27a23af18d4334ae9afb6a4b24e11a17d2e
SaneManiacMG <mmoroane@hotmail.com>
2022-12-07 09:32:53 +0200

    Moved controller implementations to service class

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
@@ -16,71 +16,37 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
     public ResponseEntity<Object> findAllUsers() {
-        try {
-            if (!userRepository.findAll().isEmpty()) {
-                return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("No users found", HttpStatus.NO_CONTENT);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error occurred looking for users", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return userService.findAllUsers();
     }
 
     @RequestMapping(value = "/findById", method = RequestMethod.POST)
     public ResponseEntity<Object> findUserById(@RequestBody User user) {
-        Optional<User> userOptional = userRepository.findById(user.getUserId());
-        try {
-            if (userOptional.isPresent()) {
-                return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("User ID not found", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred looking for user id", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return userService.findByUserId(user);
     }
 
     @RequestMapping(value = "/findByEmail", method = RequestMethod.POST)
     public ResponseEntity<Object> findUserByEmail(@RequestBody User user) {
-        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
-        try {
-            if (userOptional.isPresent()) {
-                return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Email not found", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred looking for user email", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return userService.findByEmail(user);
     }
 
     @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
-    public ResponseEntity<User> updateUser(@RequestBody User user) {
-
-        try {
-            return null;
-        } catch (Exception e) {
-            return null;
-        }
-
+    public ResponseEntity<Object> updateUser(@RequestBody User user) {
+        return userService.updateUserDetails(user);
     }
 
     @RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
-    public ResponseEntity<User> deleteUser(@RequestBody User user) {
-        return null;
+    public ResponseEntity<Object> deleteUser(@RequestBody User user) {
+        return userService.deleteUserRecord(user);
     }
 
     @PostMapping(value = "/addUser")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return null;
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        return userService.createUserRecord(user);
     }
 }
--- backend-portfolio/src/main/java/com/smworks/backendportfolio/service/UserService.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/service/UserService.java
@@ -1,10 +1,13 @@
 package com.smworks.backendportfolio.service;
 
+import com.smworks.backendportfolio.model.User;
 import com.smworks.backendportfolio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,5 +25,45 @@ public class UserService {
         } catch (Exception e) {
             return new ResponseEntity<>("Error occurred looking for users", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public ResponseEntity<Object> findByUserId(User user) {
+        Optional<User> userOptional = userRepository.findById(user.getUserId());
+        try {
+            if (userOptional.isPresent()) {
+                return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("User ID not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred performing user ID lookup",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Object> findByEmail(User user) {
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+        try {
+            if (userOptional.isPresent()) {
+                return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Email not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred looking for user email",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Object> updateUserDetails(User user) {
+        return null;
+    }
+
+    public ResponseEntity<Object> deleteUserRecord(User user) {
+        return null;
+    }
+
+    public ResponseEntity<Object> createUserRecord(User user) {
+        return null;
     }
 }
