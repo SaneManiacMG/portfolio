9acf8896d2a2fd086fc96186e2be71f2302e8c2f
SaneManiacMG <mmoroane@hotmail.com>
2022-12-07 11:28:47 +0200

    Removed Service implementation

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.smworks.backendportfolio.controller;
 
 import com.smworks.backendportfolio.model.User;
-import com.smworks.backendportfolio.service.UserService;
+import com.smworks.backendportfolio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,35 +12,43 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Object> findAllUsers() {
-        return userService.findAllUsers();
+        try {
+            if (!userRepository.findAll().isEmpty())
+                return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+            else
+                return new ResponseEntity<>("No users found", HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error has occurred retrieving result set",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @RequestMapping(value = "/findById", method = RequestMethod.POST)
     public ResponseEntity<Object> findUserById(@RequestBody User user) {
-        return userService.findByUserId(user);
+        return null;
     }
 
     @RequestMapping(value = "/findByEmail", method = RequestMethod.POST)
     public ResponseEntity<Object> findUserByEmail(@RequestBody User user) {
-        return userService.findByEmail(user);
+        return null;
     }
 
     @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
     public ResponseEntity<Object> updateUser(@RequestBody User user) {
-        return userService.updateUserDetails(user);
+        return null;
     }
 
     @RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
     public ResponseEntity<Object> deleteUser(@RequestBody User user) {
-        return userService.deleteUserRecord(user);
+        return null;
     }
 
     @PostMapping(value = "/addUser")
     public ResponseEntity<Object> createUser(@RequestBody User user) {
-        return userService.createUserRecord(user);
+        return null;
     }
 }
