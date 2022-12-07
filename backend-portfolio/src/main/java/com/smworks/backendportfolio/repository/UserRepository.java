e9e63167b575e0442e1f93ecd36a455dfa5d3ace
SaneManiacMG <mmoroane@hotmail.com>
2022-12-07 09:01:23 +0200

    Added custom method findByEmail

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/repository/UserRepository.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.smworks.backendportfolio.repository;
 
 import com.smworks.backendportfolio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByEmail(String email);
 }
