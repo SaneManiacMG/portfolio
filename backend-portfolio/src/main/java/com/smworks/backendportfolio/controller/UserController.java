e16fa3d49a71a934a2b127a509c70b5e15dc075b
SaneManiacMG <mmoroane@hotmail.com>
2022-12-07 09:01:02 +0200

    Added controls implementations for user lookups

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/UserController.java
@@ -2,22 +2,27 @@ package com.smworks.backendportfolio.controller;
 
 import com.smworks.backendportfolio.model.User;
 import com.smworks.backendportfolio.repository.UserRepository;
+import com.smworks.backendportfolio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("s")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
     public ResponseEntity<Object> findAllUsers() {
         try {
             if (!userRepository.findAll().isEmpty()) {
@@ -30,9 +35,9 @@ public class UserController {
         }
     }
 
-    @GetMapping("-id/{userId}")
-    public ResponseEntity<Object> findUserById(@PathVariable("userId") long userId) {
-        Optional<User> userOptional = userRepository.findById(userId);
+    @RequestMapping(value = "/findById", method = RequestMethod.POST)
+    public ResponseEntity<Object> findUserById(@RequestBody User user) {
+        Optional<User> userOptional = userRepository.findById(user.getUserId());
         try {
             if (userOptional.isPresent()) {
                 return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
@@ -40,26 +45,41 @@ public class UserController {
                 return new ResponseEntity<>("User ID not found", HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred looking for user", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("An error occurred looking for user id", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("-email/{email}")
-    public ResponseEntity<User> findUserByEmail(@PathVariable("email") String email) {
-        return null; //use custom record lookup using email
+    @RequestMapping(value = "/findByEmail", method = RequestMethod.POST)
+    public ResponseEntity<Object> findUserByEmail(@RequestBody User user) {
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+        try {
+            if (userOptional.isPresent()) {
+                return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Email not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred looking for user email", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<User> updateUser(@RequestParam Long userId, @RequestBody User user) {
+    @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+
+        try {
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    @RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
+    public ResponseEntity<User> deleteUser(@RequestBody User user) {
         return null;
     }
 
-    @DeleteMapping("/{userId}")
-    public ResponseEntity<User> deleteUser(@RequestParam Long userId) {
-        return null;
-    }
-
-    @PostMapping
+    @PostMapping(value = "/addUser")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return null;
     }
