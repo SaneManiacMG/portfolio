4d8f9d559dbd27395f170293fb97ff24d783a7b6
SaneManiacMG <mmoroane@hotmail.com>
2022-12-07 09:01:57 +0200

    Created findAllUsers method

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/service/UserService.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/service/UserService.java
@@ -1,7 +1,26 @@
 package com.smworks.backendportfolio.service;
 
+import com.smworks.backendportfolio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<Object> findAllUsers() {
+        try {
+            if (!userRepository.findAll().isEmpty()) {
+                return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("No users found", HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error occurred looking for users", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
