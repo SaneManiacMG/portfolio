1ecd6eb4b17cc63c9452c9a7afb535fed9176b67
SaneManiacMG <mmoroane@hotmail.com>
2022-12-08 11:18:47 +0200

    Moved User ID generation to User class

---
+++ backend-portfolio/src/main/resources/.data.sql
@@ -0,0 +1,7 @@
+insert into users
+(first_name, last_name, email, phone_nr, role, active)
+values
+('Mogomotsi', 'Moroane', 'mmoroane@hotmail.com', '0813916607', 'admin', true),
+('', 'Admin', 'dummy1@email.com', '0123456789', 'admin', true),
+('', 'user', 'dummy2@email.com', '1234567890', 'user', true),
+('', 'deactivatedAcc', 'dummy3@email.com', '1234567899', 'user', false);
--- backend-portfolio/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into users
-(user_id, first_name, last_name, email, phone_nr, role, active)
-values
-('1', 'Mogomotsi', 'Moroane', 'mmoroane@hotmail.com', '0813916607', 'admin', true),
-('2', '', 'Admin', 'dummy1@email.com', '0123456789', 'admin', true),
-('3', '', 'user', 'dummy2@email.com', '1234567890', 'user', true),
-('4', '', 'deactivatedAcc', 'dummy3@email.com', '1234567899', 'user', false);
