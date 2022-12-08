81554cc7802e61d230cc7f8f0e2cc75fa99ca2c5
SaneManiacMG <mmoroane@hotmail.com>
2022-12-08 15:51:54 +0200

    Updated .data.sql, application.properties, PortfolioBackend.postman_collection.json

--- backend-portfolio/src/main/resources/.data.sql
+++ backend-portfolio/src/main/resources/.data.sql
@@ -1,7 +1,7 @@
 insert into users
-(first_name, last_name, email, phone_nr, role, active)
+(user_id, first_name, last_name, email, phone_nr, role, active)
 values
-('Mogomotsi', 'Moroane', 'mmoroane@hotmail.com', '0813916607', 'admin', true),
-('', 'Admin', 'dummy1@email.com', '0123456789', 'admin', true),
-('', 'user', 'dummy2@email.com', '1234567890', 'user', true),
-('', 'deactivatedAcc', 'dummy3@email.com', '1234567899', 'user', false);
+('123', 'Mogomotsi', 'Moroane', 'mmoroane@hotmail.com', '0813916607', 'admin', true),
+('456', 'dummy1', 'admin', 'dummy1@email.com', '0123456789', 'admin', true),
+('789', 'dummy2', 'user', 'dummy2@email.com', '1234567890', 'user', true),
+('101', 'dummy3', 'deactivatedAcc', 'dummy3@email.com', '1234567899', 'user', false);
