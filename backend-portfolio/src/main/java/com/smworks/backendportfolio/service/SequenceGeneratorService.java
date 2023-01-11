98ddc975b556ede4d877ca604547f9df1657093f
SaneManiacMG <mmoroane@hotmail.com>
2023-01-11 11:05:34 +0200

    Impl for sequence generator

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/service/SequenceGeneratorService.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/service/SequenceGeneratorService.java
@@ -1,4 +1,24 @@
 package com.smworks.backendportfolio.service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 public class SequenceGeneratorService {
+    public String generateId() {
+        return generateNumber() + generateDateTime();
+    }
+
+    private String generateDateTime() {
+        DateTimeFormatter userIdDateTimeFormat = DateTimeFormatter.ofPattern("ssmmHHddMM");
+        LocalDateTime now = LocalDateTime.now();
+        return userIdDateTimeFormat.format(now);
+    }
+
+    private int generateNumber() {
+        Random random = new Random();
+        int upperLimit = 99;
+        int lowerLimit = 10;
+        return random.nextInt(upperLimit - lowerLimit) + lowerLimit;
+    }
 }
