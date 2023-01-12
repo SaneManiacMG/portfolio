2030733796a44f44a7fd1957decbed0bbf0ff4d3
SaneManiacMG <mmoroane@hotmail.com>
2023-01-12 15:18:34 +0200

    Changed RequestMapping to RestController

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/RegisterController.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/controller/RegisterController.java
@@ -1,7 +1,7 @@
 package com.smworks.backendportfolio.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/register")
+@RestController
 public class RegisterController {
 }
