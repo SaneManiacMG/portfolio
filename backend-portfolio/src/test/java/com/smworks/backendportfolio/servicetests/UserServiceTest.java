f1e9ada92cb6beb8765689d3f0fcaadfa6fdc9a4
SaneManiacMG <mmoroane@hotmail.com>
2023-01-23 15:00:44 +0200

    Created UserServiceImpl.java

--- backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/UserServiceTest.java
+++ backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/UserServiceTest.java
@@ -4,57 +4,48 @@ import com.smworks.backendportfolio.models.User;
 import com.smworks.backendportfolio.repositories.UserRepository;
 import com.smworks.backendportfolio.services.SequenceGeneratorService;
 import com.smworks.backendportfolio.services.UserService;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
-
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class UserServiceTest {
-    @Mock
-    private UserRepository userRepository;
-
     @Autowired
-    @InjectMocks
     private UserService userService;
-
-    private User newUser = new User();
-    private ResponseEntity<Object> listOfUsers;
+    @MockBean
+    private UserRepository userRepository;
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+    User user1;
+    User user2;
+    User user3;
 
     @BeforeEach
-    public void setup() {
-        SequenceGeneratorService sequenceGeneratorService = new SequenceGeneratorService();
-        newUser = new User(sequenceGeneratorService.generateUserId(), "TestUser1",
-                "Test", "User",
-                "testuser@email.com", "0123456789",
-                "Test", true);
-        listOfUsers = new ResponseEntity<>(userService.getAllUsers().getStatusCode());
+    public void setupData() {
+        user1 = new User(sequenceGeneratorService.generateUserId(), "SaneManiacMG",
+                "Mogomotsi", "Moroane",
+                "mmoroane@hotmail.com", "0813916607", "ADMIN",true);
+        user2 = new User(sequenceGeneratorService.generateUserId(), "dummy1admin",
+                "dummy1", "admin",
+                "dummy1@email.com", "0123456789", "ADMIN",true);
+        user3 = new User(sequenceGeneratorService.generateUserId(), "TestAcc",
+                "Test", "Acc",
+                "test@email.com", "0123456789", "TEST",true);
+
     }
 
     @AfterEach
     public void tearDown() {
-        userRepository.deleteAll();
-        newUser = null;
+        user1 = null;
+        user2 = null;
+        user3 = null;
     }
 
     @Test
-    public void getAllUsersSuccessfully() {
-        ResponseEntity<Object> listOfUsers = new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
-        String expectedResponse = HttpStatus.OK.toString() + userRepository.findAll().toString();
-        String actualResponse = listOfUsers.getStatusCode().toString() + listOfUsers.getBody().toString();
-        assertThat(expectedResponse.equals(actualResponse));
+    public void testFindAllUsers() {
+
     }
 }
