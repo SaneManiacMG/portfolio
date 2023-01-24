340eee2e21e85413f0264394fbe4872c1122127f
SaneManiacMG <mmoroane@hotmail.com>
2023-01-24 09:40:30 +0200

    Created unit test for getAllUsers() method. Previous method did not work correctly.

--- backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/UserServiceTest.java
+++ backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/UserServiceTest.java
@@ -4,24 +4,41 @@ import com.smworks.backendportfolio.models.User;
 import com.smworks.backendportfolio.repositories.UserRepository;
 import com.smworks.backendportfolio.services.SequenceGeneratorService;
 import com.smworks.backendportfolio.services.UserService;
+import com.smworks.backendportfolio.services.UserServiceImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserServiceTest {
     @Autowired
     private UserService userService;
-    @MockBean
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+    @Mock
     private UserRepository userRepository;
-    @Autowired
-    private SequenceGeneratorService sequenceGeneratorService;
+    private SequenceGeneratorService sequenceGeneratorService = new SequenceGeneratorService();
     User user1;
     User user2;
     User user3;
+    List<User> users = new ArrayList<>();
+    private Optional<User> optionalUser;
 
     @BeforeEach
     public void setupData() {
@@ -34,7 +51,9 @@ public class UserServiceTest {
         user3 = new User(sequenceGeneratorService.generateUserId(), "TestAcc",
                 "Test", "Acc",
                 "test@email.com", "0123456789", "TEST",true);
-
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
     }
 
     @AfterEach
@@ -46,6 +65,11 @@ public class UserServiceTest {
 
     @Test
     public void testFindAllUsers() {
-
+        when(userRepository.findAll()).thenReturn(users);
+        ResponseEntity<Object> response = userServiceImpl.getAllUsers();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(users, response.getBody());
     }
+
+
 }
