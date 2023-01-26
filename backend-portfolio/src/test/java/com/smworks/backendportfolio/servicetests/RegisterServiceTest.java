0855da66b37042b82d4db620bb9c80fce687239a
SaneManiacMG <mmoroane@hotmail.com>
2023-01-26 11:58:54 +0200

    No longer generating a unique sequence for userId in the user test

--- backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/RegisterServiceTest.java
+++ backend-portfolio/src/test/java/com/smworks/backendportfolio/servicetests/RegisterServiceTest.java
@@ -2,10 +2,14 @@ package com.smworks.backendportfolio.servicetests;
 
 import com.smworks.backendportfolio.models.User;
 import com.smworks.backendportfolio.repositories.UserRepository;
+import com.smworks.backendportfolio.services.RegisterService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
@@ -17,6 +21,9 @@ public class RegisterServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Autowired
+    private RegisterService registerService;
+
     @BeforeEach
     public void setup() {
         newUser = new User("GeneratedSequence", "Dummy2User", "Dummy", "Two",
@@ -24,6 +31,11 @@ public class RegisterServiceTest {
     }
 
     @Test
-    public void userDetailsExistsTest() {
+    public void registerNewUserTest() {
+        when(registerService.userDetailsExists(newUser.getUsername())).thenReturn(false);
+        when(registerService.userDetailsExists(newUser.getEmail())).thenReturn(false);
+        /*when(userRepository.findByEmail(newUser.getEmail()).isEmpty()).thenReturn(true);
+        when(userRepository.findByUsername(newUser.getUsername()).isEmpty()).thenReturn(true);*/
+        ResponseEntity<Object> response = new ResponseEntity<>("", HttpStatus.OK);
     }
 }
