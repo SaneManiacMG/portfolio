b4192855fbc335301c27b4b79dcb83b1a8d2cc45
SaneManiacMG <mmoroane@hotmail.com>
2023-01-31 11:54:03 +0200

    Removed userExists method

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/services/LoginService.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/services/LoginService.java
@@ -4,6 +4,5 @@ import com.smworks.backendportfolio.models.LoginRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface LoginService {
-    public Boolean userExists(String userIdentifier);
     public ResponseEntity<Object> authenticate(LoginRequest request);
 }
--- backend-portfolio/src/main/java/com/smworks/backendportfolio/services/LoginServiceImpl.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/services/LoginServiceImpl.java
@@ -5,6 +5,7 @@ import com.smworks.backendportfolio.models.LoginRequest;
 import com.smworks.backendportfolio.models.User;
 import com.smworks.backendportfolio.repositories.LoginRepository;
 import com.smworks.backendportfolio.repositories.UserRepository;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,40 +20,36 @@ public class LoginServiceImpl implements LoginService {
     @Autowired
     private LoginRepository loginRepository;
 
-    String userId;
-    Optional<User> user;
-
-    @Override
-    public Boolean userExists(String userIdentifier) {
-        try {
-            if (userRepository.findByUsername(userIdentifier).isPresent()) {
-                user = userRepository.findByUsername(userIdentifier);
-            } else if (userRepository.findByEmail(userIdentifier).isPresent()) {
-                user = userRepository.findByEmail(userIdentifier);
-            } else {
-                return false;
-            }
-            userId = user.get().getUserId();
-            return true;
-        } catch (Exception e) {
-            System.out.println(e);
-            return false;
-        }
-    }
     @Override
     public ResponseEntity<Object> authenticate(LoginRequest request) {
+        if (request.getPassword().isBlank() || request.getUserIdentifier().isBlank()) {
+            return new ResponseEntity<>("Not all values provided", HttpStatus.BAD_REQUEST);
+        }
 
-        if (userExists(request.getUserIdentifier())) {
-            Optional<Login> authUser = loginRepository.findById(userId);
-            if (request.getPassword().equals(authUser.get().getPassword())) {
-                return new ResponseEntity<>(authUser.get(), HttpStatus.OK);
-            } else if (request.getPassword().equals(authUser.get().getPassword()) && !authUser.get().getActive()) {
-                return new ResponseEntity<>("Account blocked", HttpStatus.UNAUTHORIZED);
+        Optional<User> existingUserByEmail = userRepository.findByEmail(request.getUserIdentifier());
+        Optional<User> existingUserByUsername = userRepository.findByUsername(request.getUserIdentifier());
+        String userId;
+
+        if (existingUserByEmail.isPresent()) {
+            userId = existingUserByEmail.get().getUserId();
+        } else if (existingUserByUsername.isPresent()) {
+            userId = existingUserByUsername.get().getUserId();
+        } else {
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
+
+        Optional<Login> loginUser = loginRepository.findById(userId);
+
+        if (loginUser.get().getActive()) {
+            Login loggedInUser = loginUser.get();
+            if (loginUser.isPresent() && request.getPassword().equals(loginUser.get().getPassword())) {
+                return new ResponseEntity<>(loggedInUser, HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Incorrect username or password", HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<>("Account locked", HttpStatus.UNAUTHORIZED);
             }
         } else {
-            return new ResponseEntity<>("Incorrect username or password", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
         }
     }
+
 }
