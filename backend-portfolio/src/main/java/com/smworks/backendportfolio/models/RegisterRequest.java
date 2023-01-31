6524824ef6a33640765d83409615790ecf9c817a
SaneManiacMG <mmoroane@hotmail.com>
2023-01-31 11:55:03 +0200

    Removed UserId from request

--- backend-portfolio/src/main/java/com/smworks/backendportfolio/models/RegisterRequest.java
+++ backend-portfolio/src/main/java/com/smworks/backendportfolio/models/RegisterRequest.java
@@ -2,10 +2,6 @@ package com.smworks.backendportfolio.models;
 
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @Component
 public class RegisterRequest {
     private String userId;
@@ -13,21 +9,13 @@ public class RegisterRequest {
     private String email;
     private String password;
 
-    public RegisterRequest(String userId, String username, String email, String password) {
+    public RegisterRequest(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
     public RegisterRequest() {
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 
     public String getUsername() {
