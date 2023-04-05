6a3a98342ab4dc36ef8267c1b92e56bf07cbb0e4
SaneManiacMG <mmoroane@hotmail.com>
2023-04-05 14:12:20 +0200

    Created Controllers, Interfaces, Services files

---
+++ backend-portfolio-csharp/backend-portfolio-csharp/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using backend_portfolio_csharp.Models;
+
+namespace backend_portfolio_csharp.Interfaces
+{
+    public interface IUserService
+    {
+        public List<User> GetAllUsers();
+        public User GetUserById(string userId);
+        public User AddUser(User user);
+        public User UpdateUser(User user);
+        public User DeleteUser(string userId);
+
+        public string GenerateUserSequence();
+
+    }
+}
---
+++ backend-portfolio-csharp/backend-portfolio-csharp/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using backend_portfolio_csharp.Interfaces;
+using backend_portfolio_csharp.Models;
+
+namespace backend_portfolio_csharp.Services
+{
+    public class UserService : IUserService
+    {
+        public User AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User DeleteUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
