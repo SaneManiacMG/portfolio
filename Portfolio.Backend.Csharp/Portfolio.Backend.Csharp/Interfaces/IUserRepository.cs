e370c8fbbdbe8d483ffefd98233721aed2aaa659
SaneManiacMG <mmoroane@hotmail.com>
2023-04-06 15:56:51 +0200

    Added IUserRepo methods Added some implementation for the methods

--- Portfolio.Backend.Csharp/Portfolio.Backend.Csharp/Interfaces/IUserRepository.cs
+++ Portfolio.Backend.Csharp/Portfolio.Backend.Csharp/Interfaces/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Portfolio.Backend.Csharp.Interfaces
+﻿using Portfolio.Backend.Csharp.Models.Entities;
+
+namespace Portfolio.Backend.Csharp.Interfaces
 {
     public interface IUserRepository
     {
+        Task<List<User>> GetUsersAsync();
+        Task<User> GetUserByIdAsync(string userId);
+        Task<User> AddUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<User> DeleteUserAsync(string userId);
     }
 }
--- Portfolio.Backend.Csharp/Portfolio.Backend.Csharp/Repositories/UserRepository.cs
+++ Portfolio.Backend.Csharp/Portfolio.Backend.Csharp/Repositories/UserRepository.cs
@@ -1,6 +1,50 @@
-﻿namespace Portfolio.Backend.Csharp.Repositories
+﻿using Portfolio.Backend.Csharp.Data;
+using Portfolio.Backend.Csharp.Interfaces;
+using Portfolio.Backend.Csharp.Models.Entities;
+
+namespace Portfolio.Backend.Csharp.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly PortfolioDbContext _dbContext;
+
+        public UserRepository(PortfolioDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> AddUserAsync(User user)
+        {
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<string> DeleteUserAsync(string userId)
+        {
+            var user = await this.GetUserByIdAsync(userId);
+            if (user == null)
+            {
+                return "UserID not found";
+            }
+            _dbContext.Users.Remove(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> GetUserByIdAsync(string userId)
+        {
+            return await _dbContext.Users.FindAsync(userId);
+        }
+
+        public Task<List<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
